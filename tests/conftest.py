6dc25cd7258ca51abbb4b36d8b02557b0178a283
Max Pfeiffer <max@maxpfeiffer.ch>
2025-05-30 08:33:36 +0200

    Enabling silly auth for docker test registry

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,7 +24,12 @@ def registry_container() -> Generator[DockerRegistryContainer, Any, None]:
 
     :return:
     """
-    with DockerRegistryContainer().with_bind_ports(5000, 5000) as registry_container:
+    with (
+        DockerRegistryContainer()
+        .with_env("REGISTRY_AUTH_SILLY_REALM", "test-realm")
+        .with_env("REGISTRY_AUTH_SILLY_SERVICE", "TEST-SERVICE")
+        .with_bind_ports(5000, 5000) as registry_container
+    ):
         yield registry_container
 
 
