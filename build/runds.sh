d91eec55b3db7f751b8a3f3c3074413c395eb552
Max Pfeiffer <max@maxpfeiffer.ch>
2025-12-26 14:58:02 +0100

    Adjusted handling of environment variables in helm chart and entrypoint

--- build/runds.sh
+++ build/runds.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`dirname $0`/RustDedicated_Data/Plugins:`dirname $0`/RustDedicated_Data/Plugins/x86_64
-
-if [[ -n "${POD_NAME}" ]]; then
-  if [[ -f "/srv/rust/config/${POD_NAME}" ]]; then
-    source "/srv/rust/config/${POD_NAME}"
+# Check for Helm chart specific files containing environment variables
+if [[ -n "${CONFIG_FILE_PATH}" ]]; then
+  if [[ -f "${CONFIG_FILE_PATH}" ]]; then
+    source "${CONFIG_FILE_PATH}"
   else
-    echo "File with environment variables at /srv/rust/config/${POD_NAME} does not exit"
+    echo "SECRET_FILE_PATH is set, but file with environment variables at ${CONFIG_FILE_PATH} does not exit"
     exit 1
   fi
 fi
 
+if [[ -n "${SECRET_FILE_PATH}" ]]; then
+  if [[ -f "${SECRET_FILE_PATH}" ]]; then
+    source "${SECRET_FILE_PATH}"
+  else
+    echo "SECRET_FILE_PATH is set, but file with environment variables at ${SECRET_FILE_PATH} does not exit"
+    exit 1
+  fi
+fi
+
+# Set Rust specific environment variables
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`dirname $0`/RustDedicated_Data/Plugins:`dirname $0`/RustDedicated_Data/Plugins/x86_64
+
+# Run Rust dedicated server
 ./RustDedicated -batchmode "$@"
