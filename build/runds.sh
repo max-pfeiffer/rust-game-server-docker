3bf442ab0c5f54ba176d5d20a624c8e0a23e5be2
Max Pfeiffer <max@maxpfeiffer.ch>
2025-12-26 12:50:17 +0100

    Mounting configMap and secret to pods filesystem

--- build/runds.sh
+++ build/runds.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`dirname $0`/RustDedicated_Data/Plugins:`dirname $0`/RustDedicated_Data/Plugins/x86_64
 
+if [[ -n "${POD_NAME}" ]]; then
+  if [[ -f "/srv/rust/config/${POD_NAME}" ]]; then
+    source "/srv/rust/config/${POD_NAME}"
+  else
+    echo "File with environment variables at /srv/rust/config/${POD_NAME} does not exit"
+    exit 1
+  fi
+fi
+
 ./RustDedicated -batchmode "$@"
